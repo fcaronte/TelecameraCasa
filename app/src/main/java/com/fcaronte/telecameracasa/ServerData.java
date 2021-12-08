c1ac313cfc0a84da3a13021302a83a38b5a43238
fraca <fra.caronte@gmail.com>
2021-12-08 13:27:29 +0100

    add missing file using generic data

---
+++ app/src/main/java/com/fcaronte/telecameracasa/ServerData.java
@@ -0,0 +1,12 @@
+package com.fcaronte.telecameracasa;
+
+public class ServerData {
+
+    /////EDIT PRIVATE STRING WITH YOUR TINYCAM WEBSERVER DATA/////////
+    public static final String user = "username";
+    public static final String password = "password";
+    public static final String address = "IP_address:port";
+    public static final String telegramBot = "yourTelegram_bot";
+    /////EDIT PRIVATE STRING WITH YOUR TINYCAM WEBSERVER DATA/////////
+
+}
